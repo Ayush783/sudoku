20418f4d12109d239e5b381abf923bd05d666d00
aayushsaajtak <aayush.sharma@aajtak.com>
2024-10-04 12:50:35 +0530

    ad service restored

--- lib/ad/service/ad_placement.dart
+++ lib/ad/service/ad_placement.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/foundation.dart';
 
 enum AdPlacement {
-  nativeBoard_1;
+  nativeBoard_1,
+  reward;
 
-  String get adUnitId => kDebugMode
-      ? 'ca-app-pub-3940256099942544/2247696110'
-      : switch (this) {
-          nativeBoard_1 => 'ca-app-pub-5712058463720831/1215778357',
-        };
+  String get adUnitId => switch (this) {
+        nativeBoard_1 => kDebugMode
+            ? 'ca-app-pub-3940256099942544/2247696110'
+            : 'ca-app-pub-5712058463720831/1215778357',
+        reward => kDebugMode
+            ? 'ca-app-pub-3940256099942544/5354046379'
+            : 'ca-app-pub-5712058463720831/5629041365',
+      };
 }

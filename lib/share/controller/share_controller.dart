a589cb5cd96f1f6868cadc8d1c427bd1f87bdfb4
aayushsaajtak <aayush.sharma@aajtak.com>
2024-10-04 18:04:34 +0530

    fixed domain in share link

--- lib/share/controller/share_controller.dart
+++ lib/share/controller/share_controller.dart
@@ -3,12 +3,12 @@ import 'package:share_plus/share_plus.dart';
 class ShareController {
   static void shareIncompleteBoard(String boardID) {
     Share.share(
-        'Hey! I\'m facing trouble solving this Sudoku puzzle, Can you help me out?\nhttps://aayushsharma.me/$boardID');
+        'Hey! I\'m facing trouble solving this Sudoku puzzle, Can you help me out?\nhttps://instantsudoku.in/$boardID');
   }
 
   static void shareCompleteBoard(String boardID, String timeElapsed) {
     Share.share(
-        'Hey! Icompleted this Sudoku puzzle in $timeElapsed, Can you beat me?\nhttps://aayushsharma.me/$boardID');
+        'Hey! Icompleted this Sudoku puzzle in $timeElapsed, Can you beat me?\nhttps://instantsudoku.in/$boardID');
   }
 
   static void shareUri(Uri uri) => Share.shareUri(uri);
